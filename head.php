cbc0a89bff007061f0d25db1dffe440aaae3a9b1
Gapy <gasper.florjan@scv.si>
2023-04-17 14:26:48 +0200

    nardiu header

--- head.php
+++ head.php
@@ -7,7 +7,13 @@
       <link rel="stylesheet" href="app.css">
 </head>
 <body>
-    <div class="header">
-        
-</div>
+<header>
+  <div class="logo">
+    <img src="slike/scvlogo_page-0001.png" alt="Your logo">
+  </div>
+  <div class="title">
+    <h1>HOME</h1>
+   
+  </div>
+</header>
 </html>
