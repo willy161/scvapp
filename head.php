b2dc526e0929ec902336addf78e371622de2daf6
Gapy <gasper.florjan@scv.si>
2023-04-17 13:29:27 +0200

    Naredu par fileov.

---
+++ head.php
@@ -0,0 +1,13 @@
+<?php include_once 'seja.php'; ?>
+<!DOCTYPE html>
+<html lang="sl">
+    <head>
+        <title>Domov</title>
+      <meta charset="UTF-8">
+      <link rel="stylesheet" href="app.css">
+</head>
+<body>
+    <div class="header">
+        
+</div>
+</html>
