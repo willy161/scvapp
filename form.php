49b8aa665e59d463e70124d7aa186dd57c3781a7
willy161 <gasper.jezovsek@gmail.com>
2023-04-17 14:19:16 +0200

    start coding

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <title>Form</title>
+    <meta charset="utf-8"/>
+    <html lang="sl">
+</head>
+<body> 
+    <form method="post" action ="confirm.php">
+    <input type="text" name="imesole" placeholder="Ime sole"><br>
+    <input type="text" name="imeucitelja" placeholder="Ime učitelja"><br>
+    <input type="number" name="stucencev" placeholder="Število učencev"><br>
+    <input type="submit">
+    </form>
+</body>
