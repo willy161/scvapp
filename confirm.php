599f2d9f5c36606cdac9aa14dfe148a856241430
willy161 <gasper.jezovsek@gmail.com>
2023-04-20 21:42:50 +0200

    insert za obisk

--- confirm.php
+++ confirm.php
@@ -1,7 +1,15 @@
-<?php
-$imesole=$_POST['imesole'];
-$imeucitelja=$_POST['imeucitelja'];
-$stucencev=$_POST['stucencev'];
-echo $imesole;
-echo $imeucitelja;
-echo $stucencev;
+<?php require_once 'povezava.php'; 
+$imesole = $_POST['imesole'];
+$imeucitelja = $_POST['imeucitelja'];
+$stucencev = $_POST['stucencev'];
+$datumobiska = $_POST['datumobiska'];
+$sql = "SELECT max(id) as id1 FROM obisk";
+$result = mysqli_query($link, $sql);
+$row = mysqli_fetch_array($result);
+$id = $row['id1'] + 1;
+$sql = "INSERT INTO obisk (id, ime_sole, ime_mentor, st_ucencev, datum_id) VALUES ('$id', '$imesole', '$imeucitelja', '$stucencev', '$datumobiska')";
+mysqli_query($link, $sql);
+$sql = "UPDATE datumi SET taken = 1 WHERE id = '$datumobiska'";
+mysqli_query($link, $sql);
+header("Location:home.php");
+?>
